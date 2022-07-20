2bbe9769cf35a5cfd5a0e7e8ee81dfe793472394
Taras <tarasmikulskii@ukr.net>
2022-07-20 18:04:26 +0300

    got rid of code repetition when working with the service, namely repeating the start of loading, completion of loading and errors, by writing our own hook for this, and rewrote MarvelService, also making it a hook

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,30 +1,24 @@
-class MarvelService {
-    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=957ce87ee4e4317e8850adec693c4d4d';
-    _baseOffset = 210;
+import {useHttp} from '../hooks/http.hook';
 
-    getResurce = async (url) => {
-        const res = await fetch(url);
+const useMarvelService = () => {
+    const {loading, request, error, clearError} = useHttp();
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status ${res.status}`);
-        }
+    const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    const _apiKey = 'apikey=957ce87ee4e4317e8850adec693c4d4d';
+    const _baseOffset = 210;
 
-        return await res.json();
-    };
-
-    getAllCharacters = async (offset = this._baseOffset) => {
-        const res = await this.getResurce(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
-        return res.data.results.map(this._transformCharacter);
+    const getAllCharacters = async (offset = _baseOffset) => {
+        const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter);
 
     }
 
-    getCharacter = async (id) => {
-        const res = await this.getResurce(`${this._apiBase}characters/${id}?&${this._apiKey}`);
-        return this._transformCharacter(res.data.results[0])
+    const getCharacter = async (id) => {
+        const res = await request(`${_apiBase}characters/${id}?&${_apiKey}`);
+        return _transformCharacter(res.data.results[0])
     }
 
-    _transformCharacter = (char) => {
+    const _transformCharacter = (char) => {
         return {
             id: char.id,
             name: char.name,
@@ -35,6 +29,8 @@ class MarvelService {
             comics: char.comics.items
         }
     }
+
+    return {loading, error, clearError, getAllCharacters, getCharacter};
 }
 
-export default MarvelService;
+export default useMarvelService;
